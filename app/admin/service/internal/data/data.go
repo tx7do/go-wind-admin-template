587522893a6b8bdd2ca9ccf029ad81666978ae5b
Bobo <yanglinbo@gmail.com>
2025-12-14 11:59:25 +0800

    feat: refactor data.

--- app/admin/service/internal/data/data.go
+++ app/admin/service/internal/data/data.go
@@ -3,9 +3,6 @@ package data
 import (
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/redis/go-redis/v9"
-
-	conf "github.com/tx7do/kratos-bootstrap/api/gen/go/conf/v1"
-	redisClient "github.com/tx7do/kratos-bootstrap/cache/redis"
 )
 
 // Data .
@@ -37,10 +34,4 @@ func NewData(
 			}
 		}
 	}, nil
-}
-
-// NewRedisClient 创建Redis客户端
-func NewRedisClient(cfg *conf.Bootstrap, logger log.Logger) *redis.Client {
-	l := log.NewHelper(log.With(logger, "module", "redis/data/admin-service"))
-	return redisClient.NewClient(cfg.Data, l)
 }
